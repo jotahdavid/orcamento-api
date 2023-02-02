957959a11081e89de3a78d58cf31ff7069e7dc6b
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-02-02 11:15:29 -0300

    feat(users): create route to get all

--- src/app/repositories/UserRepository.ts
+++ src/app/repositories/UserRepository.ts
@@ -1,6 +1,16 @@
+import fetch from 'cross-fetch';
+
+const { MOCKEND_API_URL } = process.env;
+
+if (!MOCKEND_API_URL) {
+  throw new Error('MOCKEND_API_URL variable is missing');
+}
+
 class UserRepository {
   async findAll() {
-    return [];
+    const response = await fetch(`${MOCKEND_API_URL}/users`);
+    const users = await response.json();
+    return users;
   }
 }
 
