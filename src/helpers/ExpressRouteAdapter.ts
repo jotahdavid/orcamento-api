a34de0a115c01cd0e8ee3dc872eee553fdadfb06
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-02-06 11:00:56 -0300

    feat: create ExpressRouteAdapter class

---
+++ src/helpers/ExpressRouteAdapter.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+
+interface HttpRequestQuery {
+  [key: string]: undefined | string | string[] | HttpRequestQuery | HttpRequestQuery[]
+}
+
+export interface HttpRequest {
+  body: any;
+  query: HttpRequestQuery;
+  params: Record<string, string | undefined>;
+}
+
+export interface HttpResponse {
+  statusCode: number;
+  body: any;
+}
+
+export type HttpRequestHandler = (httpRequest: HttpRequest) => HttpResponse | Promise<HttpResponse>;
+
+class ExpressRouteAdapter {
+  adapt(requestHandler: HttpRequestHandler) {
+    return async (req: Request, res: Response) => {
+      const httpRequest: HttpRequest = {
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      };
+
+      try {
+        const httpResponse = await requestHandler(httpRequest);
+        return res.status(httpResponse.statusCode).json(httpResponse.body);
+      } catch {
+        return res.sendStatus(500);
+      }
+    };
+  }
+}
+
+export default new ExpressRouteAdapter();
